c63f66c9298e8fd16d0105ac127cfe53b42e9094
Mariia Volokhonska <mvolokhonskaya@gmail.com>
2025-03-21 19:23:41 +0100

    Tests 3,4,5,6 added

--- FizzBuzzTests/FizzBuzzDetectorTests.cs
+++ FizzBuzzTests/FizzBuzzDetectorTests.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using FizzBuzz;
+using System;
 
 namespace FizzBuzzTests
 {
@@ -30,7 +31,40 @@ namespace FizzBuzzTests
             Assert.Equal("I love Fizz and Buzz Fizz they love Fizz Buzz", result.OutputString);
         }
 
+        [Fact]
+        public void TestCommasFizzBuzz()
+        {
+            string input = "I love cats, and dogs, and they love me back";
+            var result = detector.GetOverlappings(input);
+            Assert.Equal(5, result.Count);
+            Assert.Equal("I love Fizz, and Buzz, Fizz they love Fizz Buzz", result.OutputString);
+        }
 
+        [Fact]
+        public void TestLessThan7Characters()
+        {
+            string input = "I love";
+            var exception = Assert.Throws<ArgumentException>(() => detector.GetOverlappings(input));
+            Assert.Equal("An error occured: Input string doesn't meet length constraints.", exception.Message);
+        }
 
+        [Fact]
+        public void TestMoreThan100Characters()
+        {
+            string input = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            var exception = Assert.Throws<ArgumentException>(() => detector.GetOverlappings(input));
+            Assert.Equal("An error occured: Input string doesn't meet length constraints.", exception.Message);
+        }
+
+        [Fact]
+        public void TestLineBreak()
+        {
+            string input = @"I love cats and dogs
+and they love me back";
+            var result = detector.GetOverlappings(input);
+            Assert.Equal(5, result.Count);
+            Assert.Equal(@"I love Fizz and Buzz
+Fizz they love Fizz Buzz", result.OutputString);
+        }
     }
 }
